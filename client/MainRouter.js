9af3b17dfdb2e59095e3df7b2489a54fb1137a3f
Emmanuel Wachiye <wachiye25@gmail.com>
2021-10-19 17:28:09 +0300

    update paths

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
-import Home from './core/Home'
+import Home from '../client/Home'
 import Users from './user/Users'
 import Signup from './user/Signup'
 import Signin from './auth/Signin'
 import EditProfile from './user/EditProfile'
 import Profile from './user/Profile'
 import PrivateRoute from './auth/PrivateRoute'
-import Menu from './core/Menu'
+import Menu from '../client/Menu'
 
 const MainRouter = () => {
     return (<div>
