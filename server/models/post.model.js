2c0fbdc7972e1b958df56588fd22b7f08b10db20
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2023-01-04 10:16:51 +0300

    prettier code

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -1,24 +1,26 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 const PostSchema = new mongoose.Schema({
   text: {
     type: String,
-    required: 'Text is required'
+    required: "Text is required",
   },
   photo: {
     data: Buffer,
-    contentType: String
+    contentType: String,
   },
-  likes: [{type: mongoose.Schema.ObjectId, ref: 'User'}],
-  comments: [{
-    text: String,
-    created: { type: Date, default: Date.now },
-    postedBy: { type: mongoose.Schema.ObjectId, ref: 'User'}
-  }],
-  postedBy: {type: mongoose.Schema.ObjectId, ref: 'User'},
+  likes: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+  comments: [
+    {
+      text: String,
+      created: { type: Date, default: Date.now },
+      postedBy: { type: mongoose.Schema.ObjectId, ref: "User" },
+    },
+  ],
+  postedBy: { type: mongoose.Schema.ObjectId, ref: "User" },
   created: {
     type: Date,
-    default: Date.now
-  }
-})
+    default: Date.now,
+  },
+});
 
-export default mongoose.model('Post', PostSchema)
+export default mongoose.model("Post", PostSchema);
