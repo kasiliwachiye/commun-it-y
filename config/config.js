711a0e0d234adda3931dcf334e985fbc24b58044
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-08-12 18:19:42 +0300

    chore: change environment

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  env: process.env.NODE_ENV || "development",
+  env: process.env.NODE_ENV || "production",
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "communit",
-  mongoUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/defaultdb',
+  mongoUri: process.env.MONGODB_URI || "mongodb://localhost:27017/defaultdb",
 };
 
 export default config;
